26a4f69a7268ba572ee6e6077cba96b293c58143
AbdelkaderBarhoumi21 <abdelkaderbarhoumi21@gmail.com>
2026-02-03 14:51:59 +0100

    Feat: Add transactions repository interface

--- lib/features/transactions/domain/repositories/transaction_repository.dart
+++ lib/features/transactions/domain/repositories/transaction_repository.dart
@@ -4,4 +4,20 @@ import 'package:flutter_drift_advanced_project/features/transactions/domain/enti
 abstract class TransactionRepository {
   // Local operations
   ResultFuture<List<TransactionEntity>> getTransactions();
+  ResultFuture<TransactionEntity> getTransactionsById(String id);
+  ResultFuture<TransactionEntity> addTransaction(TransactionEntity transaction);
+  ResultFuture<TransactionEntity> updateTransaction(
+    TransactionEntity transaction,
+  );
+  ResultFuture<void> deleteTransaction(String id);
+
+  // Sync operations
+  ResultFuture<void> syncTransactions();
+  ResultFuture<List<TransactionEntity>> getPendingSync();
+  ResultFuture<void> markAsSynced(String id);
+  ResultFuture<void> markAsFailed(String id);
+
+  // Stream operations
+  Stream<List<TransactionEntity>> watchTransactions();
+  Stream<int> watchPendingSyncCount();
 }
